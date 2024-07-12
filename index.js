64cdc48c67a03ff3d23cbbad44ac116692c331d1
joel_patrick <jayjoelpn@gmail.com>
2024-07-13 00:11:08 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+// Find drivers whose names match the provided string (case insensitive)
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Find drivers whose names start with the provided letters
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // Find drivers whose name matches the provided string
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  
+  
